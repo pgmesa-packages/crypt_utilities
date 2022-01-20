f3fec433c89f7552a5cb06bfbfa07df4594818ff
Pablo García Mesa <pablogmesa00@gmail.com>
2022-01-20 17:40:04 +0100

    fixing where to find packages

--- setup.py
+++ setup.py
@@ -14,16 +14,17 @@ print(setuptools.find_packages())
 
 setuptools.setup(
     name='crypt_utilities',  
-    version='0.0.2',
-    package_dir={'':'src'},
+    version='0.0.1',
     author="Pablo García Mesa",
     author_email="pgmesa.sm@gmail.com",
     description="An easy and simplified cryptographic utility package (fernet, RSA, hashes...)",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/pgmesa-packages/crypt_utilities",
-    packages=setuptools.find_packages(),
+    package_dir={'': 'src'},
+    packages=setuptools.find_packages(where='src'),
     install_requires=requirements,
+    python_requires=">=3.7",
     classifiers=[
         "Programming Language :: Python :: 3.7",
         "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
